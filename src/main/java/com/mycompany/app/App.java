5f1b66fed7711ebabd00ac0d9d48698a5635f910
Seow Wei Wen <FactoryOnlyUsesEmail@gmail.com>
2023-02-27 08:35:03 +0000

    trigger githook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Too many things to learn");
+        System.out.println("Endingsoon");
     }
 
     public String getMessage() {
