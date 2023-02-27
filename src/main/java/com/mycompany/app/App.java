7978ad8560b5bb75f5b41a9c40c6775b7b5e2763
Seow Wei Wen <FactoryOnlyUsesEmail@gmail.com>
2023-02-27 07:56:51 +0000

    trigger compile

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("So many things to learn");
+        System.out.println("Too many things to learn");
     }
 
     public String getMessage() {
