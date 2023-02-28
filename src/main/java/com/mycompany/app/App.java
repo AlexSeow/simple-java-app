343070c1e0a2573412ca6ec258b9e38901f57b10
Seow Wei Wen <FactoryOnlyUsesEmail@gmail.com>
2023-02-28 05:43:38 +0000

    7th change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Retry2");
+        System.out.println("Retry3");
     }
 
     public String getMessage() {
